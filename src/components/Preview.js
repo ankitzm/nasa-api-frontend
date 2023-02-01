0c9b628a033eab1fee36b063cc251bbf05cc9895
Ankit Singh <ankit.singh.zm@gmail.com>
2023-02-01 17:16:54 +0530

    fixed api varible name

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -41,7 +41,7 @@ function Preview({ mediaType, url, title, date, explanation, copyright }) {
 				{showModal ? (
 					<>
 						<div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none ">
-							<div className="relative w-auto my-6 mx-auto w-10/12">
+							<div className="relative my-6 mx-auto w-10/12">
 								{/*content*/}
 								<div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
 							
